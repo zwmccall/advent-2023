fab475a11c5d8ee05c5412b12ea18d71240ef493
zwmccall <32351629+zwmccall@users.noreply.github.com>
2023-12-07 00:04:07 -0600

    Finished part 1
    Fixed issue where numbers weren't being cleared if they ended at the end of the line. Need to go back and make all clear at once instead of number by number

--- Day_3/solution.py
+++ Day_3/solution.py
@@ -1,6 +1,6 @@
 import re
 
-with open("Day_3/example_1.txt", "r") as input_file:
+with open("Day_3/input.txt", "r") as input_file:
 	lines = input_file.readlines()
 
 def part_1():
@@ -8,7 +8,6 @@ def part_1():
 	for line in lines:
 		x = list(line.strip())
 		array.append(x)
-	print(array)
 	for idx, line in enumerate(array):
 		is_partnum = False
 		temp_array = []
@@ -34,18 +33,31 @@ def part_1():
 					if not jdx == 0:
 						if not array[idx+1][jdx-1].isdigit() and not array[idx+1][jdx-1] == '.':
 							is_partnum = True
-					if not array[idx-1][jdx].isdigit() and not array[idx-1][jdx] == '.':
+					if not array[idx+1][jdx].isdigit() and not array[idx+1][jdx] == '.':
 						is_partnum = True
 					if not jdx == len(line) - 1:
 						if not array[idx+1][jdx+1].isdigit() and not array[idx+1][jdx+1] == '.':
 							is_partnum = True
 			else:
 				if not is_partnum:
-					for i in temp_array: array[i[0]][i[1]] = '.'
+					for num_indexes in temp_array: array[num_indexes[0]][num_indexes[1]] = '.'
 				is_partnum = False
 				temp_array = []
-			#print(idx, jdx)
-	return str(array)
+		if not is_partnum:
+			for num_indexes in temp_array: array[num_indexes[0]][num_indexes[1]] = '.'
+
+	new = "" 
+
+	# traverse in the string 
+	for row in array:
+		for char in row:
+			new += char 
+	numbers = re.findall(r'\d+', new)
+	sum = 0
+	for number in numbers:
+		sum += int(number)
+	
+	return str(sum)
 
 print('Part 1: ' + part_1())
 
