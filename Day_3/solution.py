4959760870946f86be6e78dc903f0ca376901bcc
zwmccall <32351629+zwmccall@users.noreply.github.com>
2023-12-06 23:16:32 -0600

    Flipped the backwards logic

--- Day_3/solution.py
+++ Day_3/solution.py
@@ -10,56 +10,39 @@ def part_1():
 		array.append(x)
 	print(array)
 	for idx, line in enumerate(array):
-		mid_digit = False
+		is_partnum = False
 		temp_array = []
 		for jdx, element in enumerate(line):
 			if element.isdigit():
 				temp_array.append([idx,jdx])
-				if mid_digit:
-					array[idx][jdx] = '.'
 				if not idx == 0:
 					if not jdx == 0:
 						if not array[idx-1][jdx-1].isdigit() and not array[idx-1][jdx-1] == '.':
-							array[idx][jdx] = '.'
-							mid_digit = True
-							for i in temp_array: array[i[0]][i[1]] = '.'
+							is_partnum = True
 					if not array[idx-1][jdx].isdigit() and not array[idx-1][jdx] == '.':
-						array[idx][jdx] = '.'
-						mid_digit = True
-						for i in temp_array: array[i[0]][i[1]] = '.'
+						is_partnum = True
 					if not jdx == len(line) - 1:
 						if not array[idx-1][jdx+1].isdigit() and not array[idx-1][jdx+1] == '.':
-							array[idx][jdx] = '.'
-							mid_digit = True
-							for i in temp_array: array[i[0]][i[1]] = '.'
+							is_partnum = True
 				if not jdx == 0:
 					if not array[idx][jdx-1].isdigit() and not array[idx][jdx-1] == '.':
-						array[idx][jdx] = '.'
-						mid_digit = True
-						for i in temp_array: array[i[0]][i[1]] = '.'
+						is_partnum = True
 				if not jdx == len(line) - 1:
 					if not array[idx][jdx+1].isdigit() and not array[idx][jdx+1] == '.':
-						array[idx][jdx] = '.'
-						mid_digit = True
-						for i in temp_array: array[i[0]][i[1]] = '.'
+						is_partnum = True
 				if not idx == len(array) - 1:
 					if not jdx == 0:
 						if not array[idx+1][jdx-1].isdigit() and not array[idx+1][jdx-1] == '.':
-							array[idx][jdx] = '.'
-							mid_digit = True
-							for i in temp_array: array[i[0]][i[1]] = '.'
+							is_partnum = True
 					if not array[idx-1][jdx].isdigit() and not array[idx-1][jdx] == '.':
-						array[idx][jdx] = '.'
-						mid_digit = True
-						for i in temp_array: array[i[0]][i[1]] = '.'
+						is_partnum = True
 					if not jdx == len(line) - 1:
 						if not array[idx+1][jdx+1].isdigit() and not array[idx+1][jdx+1] == '.':
-							print(temp_array[0][1])
-							array[idx][jdx] = '.'
-							mid_digit = True
-							for i in temp_array: array[i[0]][i[1]] = '.'
+							is_partnum = True
 			else:
-				mid_digit = False
+				if not is_partnum:
+					for i in temp_array: array[i[0]][i[1]] = '.'
+				is_partnum = False
 				temp_array = []
 			#print(idx, jdx)
 	return str(array)
